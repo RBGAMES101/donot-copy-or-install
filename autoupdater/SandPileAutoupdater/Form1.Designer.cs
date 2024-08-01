62ece70a0161e88960ac443b6343b9acf79635d7
Mihajlo Radojković <kulmika4@gmail.com>
2024-08-01 02:47:16 +0200

    add autoupdater

---
+++ autoupdater/SandPileAutoupdater/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SandPileAutoupdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(282, 227);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Location = new System.Drawing.Point(12, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(282, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Checking for updates...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 271);
+            this.progressBar1.MarqueeAnimationSpeed = 10;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(282, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(306, 306);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ autoupdater/SandPileAutoupdater/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using HttpClientProgress;
+using System.Security.Policy;
+using System.Diagnostics;
+
+namespace SandPileAutoupdater
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient httpClient = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            string appdata = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string[] args = Environment.GetCommandLineArgs();
+            Uri url = new Uri(args.Last());
+
+            await CheckForUpdate(appdata);
+
+            label1.Text = "Launching player...";
+            Process.Start(appdata + "/SandPile/Player.exe", url.AbsolutePath.Substring(1));
+
+            Application.Exit();
+        }
+
+        private async Task CheckForUpdate(string appdata)
+        {
+
+            Directory.CreateDirectory(appdata + "/SandPile/");
+
+
+            string latestVer = await httpClient.GetStringAsync("https://sandpile.xyz/api/GetClientVersion");
+
+            string currentVer;
+            try
+            {
+                currentVer = File.ReadAllText(appdata + "/SandPile/version");
+            }
+            catch (Exception)
+            {
+                currentVer = "0";
+            }
+
+            if (!File.Exists(appdata + "/SandPile/Player.exe") || currentVer != latestVer)
+            {
+                var progress = new Progress<float>();
+
+                label1.Text = "Downloading update...";
+                progressBar1.Style = ProgressBarStyle.Blocks;
+
+                progress.ProgressChanged += Progress_ProgressChanged;
+
+                using (var file = new FileStream(appdata + "/SandPile/Player.exe", FileMode.Create, FileAccess.Write, FileShare.None))
+                    await httpClient.DownloadDataAsync("https://sandpile.xyz/static/Player.exe", file, progress);
+
+                File.WriteAllText(appdata + "/SandPile/version", latestVer);
+
+                progressBar1.Value = 0;
+                progressBar1.Style = ProgressBarStyle.Marquee;
+            }
+
+        }
+
+        void Progress_ProgressChanged(object sender, float progress)
+        {
+            progressBar1.Value = Math.Min((int)(progress * 100), 99);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+
+}
